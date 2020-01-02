efb4c0b9f49f9adadaa116cad8922f5978d4dfbc
Arjen Wiersma <arjen@wiersma.org>
2020-01-02 21:46:20 +0100

    2015 Day 03

---
+++ 2015/src/main/java/com/buildfunthings/aoc/days/Day03.java
@@ -0,0 +1,91 @@
+package com.buildfunthings.aoc.days;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.buildfunthings.aoc.common.Day;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+public class Day03 implements Day {
+    @AllArgsConstructor
+    @ToString
+    @EqualsAndHashCode
+    class Position {
+        int x, y;
+    }
+
+    private Map<Position, Integer> performSteps(List<String> steps) {
+        Map<Position, Integer> locations = new HashMap<>();
+
+        int x = 0, y = 0;
+
+        for (String step : steps) {
+            switch (step) {
+            case "<":
+                --x;
+                break;
+            case">":
+                ++x;
+                break;
+            case "^":
+                ++y;
+                break;
+            case "v":
+                --y;
+                break;
+            }
+
+            Position p = new Position(x, y);
+            Integer currentValue = locations.get(p);
+            locations.put(p, (currentValue == null ? 1 : currentValue++));
+        }
+
+        return locations;
+    }
+
+    @Override
+    public String part1(List<String> input) {
+        List<String> tokens = new ArrayList<>(Arrays.asList(input.get(0).split("")));
+
+        Map<Position, Integer> locations = new HashMap<>();
+        locations.put(new Position(0, 0), 1);
+
+        locations.putAll(performSteps(tokens));
+
+        return String.valueOf(locations.size());
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        List<String> tokens = new ArrayList<>(Arrays.asList(input.get(0).split("")));
+
+        Map<Position, Integer> locations = new HashMap<>();
+        locations.put(new Position(0, 0), 1);
+
+        List<String> santaInstructions = IntStream
+            .range(0, tokens.size())
+            .filter(n -> n % 2 == 0)
+            .mapToObj(tokens::get)
+            .collect(Collectors.toList());
+
+        List<String> roboInstructions = IntStream
+            .range(0, tokens.size())
+            .filter(n -> n % 2 != 0)
+            .mapToObj(tokens::get)
+            .collect(Collectors.toList());
+
+        locations.putAll(performSteps(santaInstructions));
+        locations.putAll(performSteps(roboInstructions));
+
+        return String.valueOf(locations.size());
+    }
+
+}
---
+++ 2015/src/test/java/com/buildfunthings/aoc/days/Day03Test.java
@@ -0,0 +1,66 @@
+package com.buildfunthings.aoc.days;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.buildfunthings.aoc.common.Day;
+import com.buildfunthings.aoc.common.DayInputExternalResource;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class Day03Test {
+
+    @Rule
+    public DayInputExternalResource input = new DayInputExternalResource(03);
+
+    @Test
+    public void testSampleInput1() {
+        Map<String, String> tests = new HashMap<>() {
+            {
+                put(">", "2");
+                put("^>v<", "4");
+                put("^v^v^v^v^v", "2");
+            }
+        };
+
+        for (String k : tests.keySet()) {
+            Day day = new Day03();
+            assertEquals(tests.get(k), day.part1(new ArrayList<String>() {
+                {
+                    add(k);
+                }
+            }));
+        }
+    }
+
+    @Test
+    public void testSampleInput2() {
+        Map<String, String> tests = new HashMap<>() {
+            {
+                put("^v", "3");
+                put("^>v<", "3");
+                put("^v^v^v^v^v", "11");
+            }
+        };
+
+        for (String k : tests.keySet()) {
+            Day day = new Day03();
+            assertEquals(tests.get(k), day.part2(new ArrayList<String>() {
+                {
+                    add(k);
+                }
+            }));
+        }
+    }
+
+    @Test
+    public void testRealInput() {
+        Day day = new Day03();
+        assertEquals("2081", day.part1(input.getLines()));
+        assertEquals("2341", day.part2(input.getLines()));
+    }
+}
