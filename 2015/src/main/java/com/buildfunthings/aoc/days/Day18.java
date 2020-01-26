bb38fa4a2f75bafc00fbdce2952e1df73cd4e8d9
Arjen Wiersma <arjen@wiersma.org>
2020-01-26 19:36:13 +0100

    2015 day 18 iterative solution

---
+++ 2015/src/main/java/com/buildfunthings/aoc/days/Day18.java
@@ -0,0 +1,122 @@
+package com.buildfunthings.aoc.days;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import com.buildfunthings.aoc.common.Day;
+
+public class Day18 implements Day {
+
+    int[][] createGrid(int size) {
+        return new int[size][size];
+    }
+
+    void setGridState(int[][] grid, List<String> state) {
+        for (int i = 0; i < state.size(); i++) {
+            int[] chars = state.get(i).chars().toArray();
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] == '#') {
+                    grid[i][j] = 1;
+                } else {
+                    grid[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    void printGrid(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                switch (grid[i][j]) {
+                case 1:
+                    System.out.print("#");
+                    break;
+                default:
+                    System.out.print(".");
+                }
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    int[][] evolveGrid(int[][] grid, int part) {
+        int[][] evolved = new int[grid.length][grid.length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                int on = 0;
+                // cycle neighbors
+                for (int row = Math.max(0, i - 1); row <= Math.min(grid.length - 1, i + 1); row++) {
+                    for (int col = Math.max(0, j - 1); col <= Math.min(grid.length - 1, j + 1); col++) {
+                        if (row != i || col != j) {
+
+                            switch (grid[row][col]) {
+                            case 1:
+                                ++on;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (grid[i][j] == 1 && (on == 2 || on == 3)) {
+                    evolved[i][j] = 1;
+                } else if (grid[i][j] == 0 && on == 3) {
+                    evolved[i][j] = 1;
+                } else {
+                    evolved[i][j] = 0;
+                }
+
+                if (part == 2) {
+                    evolved[0][0] = 1;
+                    evolved[0][evolved.length-1] = 1;
+                    evolved[evolved.length-1][0] = 1;
+                    evolved[evolved.length-1][evolved.length-1] = 1;                    
+                }
+            }
+        }
+        return evolved;
+    }
+
+    int countOn(int[][] grid) {
+        int on = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1)
+                    ++on;
+            }
+        }
+        return on;
+    }
+
+    @Override
+    public String part1(List<String> input) {
+        int[][] grid = createGrid(100);
+        setGridState(grid, input);
+        // printGrid(grid);
+        for (int i = 0; i < 100; i++) {
+            // System.out.println();
+            int[][] ev1 = evolveGrid(grid,1);
+            // printGrid(ev1);
+            grid = ev1;
+        }
+
+        return String.valueOf(countOn(grid));
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        int[][] grid = createGrid(100);
+        setGridState(grid, input);
+        // printGrid(grid);
+        for (int i = 0; i < 100; i++) {
+            // System.out.println();
+            int[][] ev1 = evolveGrid(grid,2);
+            // printGrid(ev1);
+            grid = ev1;
+        }
+
+        return String.valueOf(countOn(grid));
+    }
+
+}
---
+++ 2015/src/test/java/com/buildfunthings/aoc/days/Day18Test.java
@@ -0,0 +1,78 @@
+package com.buildfunthings.aoc.days;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.buildfunthings.aoc.common.Day;
+import com.buildfunthings.aoc.common.DayInputExternalResource;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class Day18Test {
+
+    @Rule
+    public DayInputExternalResource input = new DayInputExternalResource(18);
+
+    @Test
+    public void testSampleInput() {
+        List<String> input = new ArrayList<>() {
+            {
+                add(".#.#.#");
+                add("...##.");
+                add("#....#");
+                add("..#...");
+                add("#.#..#");
+                add("####..");
+            }
+        };
+
+        Day18 d = new Day18();
+        int[][] grid = d.createGrid(6);
+        d.setGridState(grid, input);
+        //d.printGrid(grid);
+        for (int i = 0; i < 5; i++) {
+            //System.out.println();
+            int[][] ev1 = d.evolveGrid(grid, 1);
+            //d.printGrid(ev1);
+            grid = ev1;
+        }
+        assertEquals(4, d.countOn(grid));
+    }
+
+    @Test
+    public void testSampleInput2() {
+        List<String> input = new ArrayList<>() {
+            {
+                add("##.#.#");
+                add("...##.");
+                add("#....#");
+                add("..#...");
+                add("#.#..#");
+                add("####.#");
+            }
+        };
+
+        Day18 d = new Day18();
+        int[][] grid = d.createGrid(6);
+        d.setGridState(grid, input);
+        //d.printGrid(grid);
+        for (int i = 0; i < 5; i++) {
+            //System.out.println();
+            int[][] ev1 = d.evolveGrid(grid, 2);
+            //d.printGrid(ev1);
+            grid = ev1;
+        }
+
+        assertEquals(17, d.countOn(grid));
+    }
+
+    @Test
+    public void testRealInput() {
+        Day day = new Day18();
+        assertEquals("768", day.part1(input.getLines()));
+        assertEquals("781", day.part2(input.getLines()));
+    }
+}
