5b0652e0b346801af33b33d05251b3c204ef808f
Arjen Wiersma <arjen@wiersma.org>
2021-11-10 14:57:12 +0100

    :heavy-check-mark: fix build pipeline

--- 2015/src/main/java/com/buildfunthings/aoc/days/Day22.java
+++ 2015/src/main/java/com/buildfunthings/aoc/days/Day22.java
@@ -1,34 +1,27 @@
 package com.buildfunthings.aoc.days;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import com.buildfunthings.aoc.common.Day;
 
-import javafx.scene.effect.Effect;
-
 public class Day22 implements Day<Integer> {
 
-    /*
-     * Implement a GameState object; it contains the stats for both players (in a
-     * Player object), a list of attacks performed by the wizard and a list of
-     * effects on the Player.
-     * 
-     * After a turn the state is evaluated, if the GameState is final the game is
-     * over.
-     * 
-     * There is a queue with solvable GameState objects. The solver takes a state
-     * from the queue and applies the effects, then it performs an attack.
-     * 
-     * On a turn for the boss it applies the effects and attacks. The new game state
-     * is added to the queue.
-     * 
-     * On a turn for the wizard create GameState for each of the spells and performs
-     * each one; it applies the effects and attacks. This means that each
-     * combination of magic is evaluated. The resulting Game state is added to the
-     * queue.
-     */
+    @FunctionalInterface
+    interface TriFunction<A, B, C, R> {
+
+        R apply(A a, B b, C c);
+
+        default <V> TriFunction<A, B, C, V> andThen(Function<? super R, ? extends V> after) {
+            Objects.requireNonNull(after);
+            return (A a, B b, C c) -> after.apply(apply(a, b, c));
+        }
+    }
 
     class GameState {
         Player me;
@@ -41,42 +34,70 @@ public class Day22 implements Day<Integer> {
 
     class Attack {
         String name;
-        //         SENDER  RECEIVER Kill?
-        BiFunction<Player, Player,  Boolean> func;
-        int lifetime = 1;
+        // SENDER RECEIVER Return: Kill?
+        BiFunction<Player, Player, Boolean> func;
+        Player sender;
+        Player receiver;
+        int cost;
+        int lifetime = 1; // How many turns? 1 means only a single attack
+        boolean instant = false; // Do immediate damage, or delayed?
 
-        public Attack(String name, BiFunction<Player, Player, Boolean> func, int lifetime) {
+        public Attack(String name, BiFunction<Player, Player, Boolean> func, Player send, Player recv, int lifetime,
+                boolean instant, int cost) {
             this.name = name;
             this.func = func;
             this.lifetime = lifetime;
+            this.instant = instant;
+            this.sender = send;
+            this.receiver = recv;
+            this.cost = cost;
         }
 
-        
     }
-    
-    class Player {
-        int mana = 10;
-        int health = 100;
 
+    class Player {
+        int mana = 500;
+        int health = 100;
+        int armor = 0;
     }
-    
+
     public GameState newGameState() {
         GameState gs = new GameState();
 
         Player p = new Player();
 
-        BiFunction<Player, Player, Attack> missile = (s, r) -> new Attack("Massive Missile", (sender, receiver) -> {
-            --receiver.health;
-            return receiver.health <= 0;
-        }, 1);
-        
+        Map<String, BiFunction<Player, Player, Attack>> attacks = new HashMap<>() {
+            {
+                put(("Massive Missile"), (s, r) -> new Attack("Massive Missile", (sender, receiver) -> {
+                    r.health -= 4;
+                    return r.health <= 0;
+                }, s, r, 1, true, 53));
+                put(("Drain"), (s, r) -> new Attack("Drain", (sender, receiver) -> {
+                    s.health += 2;
+                    r.health -= 2;
+                    return r.health <= 0;
+                }, s, r, 1, true, 73));
+                put(("Shield"), (s, r) -> new Attack("Shield", (sender, receiver) -> {
+                    s.armor += 7;
+                    return r.health <= 0;
+                }, s, r, 6, false, 113));
+                put(("Poison"), (s, r) -> new Attack("Poison", (sender, receiver) -> {
+                    r.health -= 3;
+                    return r.health <= 0;
+                }, s, r, 6, false, 173));
+                put(("Recharge"), (s, r) -> new Attack("Recharge", (sender, receiver) -> {
+                    s.mana += 101;
+                    return r.health <= 0;
+                }, s, r, 5, false, 229));
+            }
+        };
+
         return gs;
     }
-    
+
     @Override
     public Integer part1(List<String> input) {
 
-        
         return null;
     }
 
