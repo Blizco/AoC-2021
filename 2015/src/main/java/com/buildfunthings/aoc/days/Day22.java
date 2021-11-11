848d8d4e823a09eb4981b89261149070ffb165aa
Arjen Wiersma <arjen@wiersma.org>
2021-11-11 09:07:44 +0100

    :snail: Adding queues to Day 22

--- 2015/src/main/java/com/buildfunthings/aoc/days/Day22.java
+++ 2015/src/main/java/com/buildfunthings/aoc/days/Day22.java
@@ -1,25 +1,62 @@
 package com.buildfunthings.aoc.days;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import com.buildfunthings.aoc.common.Day;
 
-
 public class Day22 implements Day<Integer> {
+    enum AttackEnum {
+        MASSIVE_MISSILE(true, 53, 4, 0, 1);
+
+        int cost; // cost of the attack in mana
+        int armor; // armor change from attack
+        int damage; // damage done by the attack
+        int lifetime; // how many turns does it stay?
+        boolean instant; // is the attack instant or on the next turn
+
+        private AttackEnum(boolean instant, int cost, int damage, int armor, int lifetime) {
+            this.cost = cost;
+            this.armor = armor;
+            this.damage = damage;
+            this.instant = instant;
+            this.lifetime = lifetime;
+        }
+
+    }
+
+    class Attack {
+        int turns; // counter how many times this attack has been played
+        AttackEnum type; // what attack is it?
+
+        boolean isActive() {
+            return false;
+        }
+    }
+
+    class Game implements Cloneable {
+
+        // Player stats
+        // Boss stats
+        // List of attacks ()
+        @Override
+        protected Game clone() throws CloneNotSupportedException {
+            return (Game) super.clone();
+        }
+
+    }
 
     
     @Override
     public Integer part1(List<String> input) {
+        Queue<Integer> q = new PriorityQueue<>();
 
-        return null;
+        return 0;
     }
 
     @Override
     public Integer part2(List<String> input) {
-        return null;
+        return 0;
     }
-
 }
--- 2015/src/test/java/com/buildfunthings/aoc/days/Day22Test.java
+++ 2015/src/test/java/com/buildfunthings/aoc/days/Day22Test.java
@@ -1,7 +1,6 @@
 package com.buildfunthings.aoc.days;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import com.buildfunthings.aoc.common.DayInputExternalResource;
 
@@ -13,16 +12,10 @@ public class Day22Test {
     public DayInputExternalResource input = new DayInputExternalResource(21);
 
     @Test
-    public void testSampleInput() {
-
-        // Day22.Player p1 = new Day22.Player();
-        // p1.mana = 100;
-        // Day22.Player p2 = new Day22.Player();
-        // p2.mana = 200;
-        
-        // Day22.Attack a = new Day22.Attack("Massive Missile", (s, r) -> {
-        //         r.health -= 4;
-        //         return r.health <= 0;
-        // }, p1, p2, 1, true, 53);
+    public void testRealInput() {
+        Day22 day = new Day22();
+        assertEquals(Integer.valueOf(0), day.part1(input.getLines()));
+        assertEquals(Integer.valueOf(0), day.part2(input.getLines()));
     }
+
 }
