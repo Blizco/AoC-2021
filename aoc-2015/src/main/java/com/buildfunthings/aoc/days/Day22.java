7b6995ac66a37e229e4024a2a46058f88f4f00e7
Arjen Wiersma <arjen@wiersma.org>
2021-11-14 15:59:17 +0100

    :sparkles: Day 22 solved

--- aoc-2015/src/main/java/com/buildfunthings/aoc/days/Day22.java
+++ aoc-2015/src/main/java/com/buildfunthings/aoc/days/Day22.java
@@ -1,62 +1,124 @@
 package com.buildfunthings.aoc.days;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.buildfunthings.aoc.common.Day;
 
 public class Day22 implements Day<Integer> {
-    enum AttackEnum {
-        MASSIVE_MISSILE(true, 53, 4, 0, 1);
 
-        int cost; // cost of the attack in mana
-        int armor; // armor change from attack
-        int damage; // damage done by the attack
-        int lifetime; // how many turns does it stay?
-        boolean instant; // is the attack instant or on the next turn
+    private static int solve(List<String> s, boolean hard) {
+        PriorityQueue<Wizard> wizards = new PriorityQueue<Wizard>((a, b) -> Integer.compare(b.mana_spend, a.mana_spend));
+        AtomicInteger minMana = new AtomicInteger(Integer.MAX_VALUE);
+        wizards.add(new Wizard(50, 500, parseBoss(s)));
+        while (wizards.size() > 0) {
+            Wizard curr = wizards.poll();
+            if (hard) {
+                if (curr.hp-- <= 0)
+                    continue;
+            }
+            curr.applyEffect();
+            for (int spell = 0; spell < Wizard.spells.length; spell++) {
+                if (curr.canCast(spell)) {
+                    Wizard next = curr.clone();
+                    next.castSpell(spell);
+                    next.applyEffect();
 
-        private AttackEnum(boolean instant, int cost, int damage, int armor, int lifetime) {
-            this.cost = cost;
-            this.armor = armor;
-            this.damage = damage;
-            this.instant = instant;
-            this.lifetime = lifetime;
+                    if (next.boss[0] <= 0) {
+                        minMana.set(Math.min(minMana.get(), next.mana_spend));
+                        wizards.removeIf(w -> w.mana_spend > minMana.get());
+                    } else {
+                        next.hp -= Math.max(1, next.boss[1] - next.armor);
+                        if (next.hp > 0 && next.mana > 0 && next.mana_spend < minMana.get())
+                            wizards.add(next);
+                    }
+                }
+            }
         }
-
+        return minMana.get();
     }
 
-    class Attack {
-        int turns; // counter how many times this attack has been played
-        AttackEnum type; // what attack is it?
-
-        boolean isActive() {
-            return false;
+    private static int[] parseBoss(List<String> s) {
+        int[] boss = new int[2];
+        for (int i = 0; i < boss.length; i++) {
+            boss[i] = Integer.valueOf(s.get(i).split(": ")[1]);
         }
+        return boss;
     }
 
-    class Game implements Cloneable {
-
-        // Player stats
-        // Boss stats
-        // List of attacks ()
-        @Override
-        protected Game clone() throws CloneNotSupportedException {
-            return (Game) super.clone();
-        }
-
+    public static void main(String[] args) throws IOException {
     }
 
-    
     @Override
     public Integer part1(List<String> input) {
-        Queue<Integer> q = new PriorityQueue<>();
-
-        return 0;
+        return solve(input, false);
+        // long start = System.currentTimeMillis();
+        // System.out.println("Part One = " + solve(input, false));
+        // System.out.println(System.currentTimeMillis() - start);
+        // return 0;
     }
 
     @Override
     public Integer part2(List<String> input) {
-        return 0;
+        return solve(input, true);
+    }
+}
+
+class Wizard implements Cloneable {
+
+    static int[][] spells = { { 53, 0 }, { 73, 0 }, { 113, 6 }, { 173, 6 }, { 229, 5 } };
+
+    int hp, mana, armor, mana_spend;
+
+    int[] active_effects = new int[3];
+    int[] boss; // {hp, dmg}
+
+    public Wizard(int hp, int mana, int[] boss) {
+        this.hp = hp;
+        this.mana = mana;
+        this.boss = boss;
+    }
+
+    public boolean canCast(int i) {
+        return mana >= spells[i][0] && (i < 2 || active_effects[i - 2] == 0);
+    }
+
+    public void castSpell(int i) {
+        mana -= spells[i][0];
+        mana_spend += spells[i][0];
+        if (i == 0) { // Magic Missile
+            boss[0] -= 4;
+        } else if (i == 1) { // Drain
+            hp += 2;
+            boss[0] -= 2;
+        } else { // active effect
+            active_effects[i - 2] = spells[i][1];
+        }
+    }
+
+    public void applyEffect() {
+        for (int i = 0; i < active_effects.length; i++) {
+            if (active_effects[i] > 0) {
+                active_effects[i]--;
+                if (i == 0) { // Shield
+                    armor = 7;
+                } else if (i == 1) { // Poison
+                    boss[0] -= 3;
+                } else if (i == 2) { // Recharge
+                    mana += 101;
+                }
+            } else if (i == 0)
+                armor = 0;
+        }
+    }
+
+    public Wizard clone() {
+        Wizard neu = new Wizard(hp, mana, boss.clone());
+        neu.armor = this.armor;
+        neu.mana_spend = this.mana_spend;
+        neu.active_effects = this.active_effects.clone();
+        return neu;
     }
 }
--- aoc-2015/src/test/java/com/buildfunthings/aoc/days/Day22Test.java
+++ aoc-2015/src/test/java/com/buildfunthings/aoc/days/Day22Test.java
@@ -9,13 +9,13 @@ import org.junit.Test;
 
 public class Day22Test {
     @Rule
-    public DayInputExternalResource input = new DayInputExternalResource(21);
+    public DayInputExternalResource input = new DayInputExternalResource(22);
 
     @Test
     public void testRealInput() {
         Day22 day = new Day22();
-        assertEquals(Integer.valueOf(0), day.part1(input.getLines()));
-        assertEquals(Integer.valueOf(0), day.part2(input.getLines()));
+        assertEquals(Integer.valueOf(1269), day.part1(input.getLines()));
+        assertEquals(Integer.valueOf(1309), day.part2(input.getLines()));
     }
 
 }
