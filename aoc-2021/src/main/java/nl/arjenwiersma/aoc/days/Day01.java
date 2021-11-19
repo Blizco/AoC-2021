210b8cd92a0d00e9204726280b2998d47bc030d6
Arjen Wiersma <arjen@wiersma.org>
2021-11-19 10:45:32 +0100

    :tada: Preparations for AOC 2021

---
+++ aoc-2021/src/main/java/nl/arjenwiersma/aoc/days/Day01.java
@@ -0,0 +1,19 @@
+package nl.arjenwiersma.aoc.days;
+
+import java.util.List;
+
+import nl.arjenwiersma.aoc.common.Day;
+
+public class Day01 implements Day<Integer> {
+
+    @Override
+    public Integer part1(List<String> input) {
+        return 0;
+    }
+
+    @Override
+    public Integer part2(List<String> input) {
+        return 0;
+    }
+    
+}
