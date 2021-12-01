dbccdec4cdeb39a27f3a00b2d5e585252f56389e
Arjen Wiersma <arjen@wiersma.org>
2021-12-01 08:38:43 +0100

    :sparkles: Start the AOC!

--- aoc-2021/src/main/java/nl/arjenwiersma/aoc/days/Day01.java
+++ aoc-2021/src/main/java/nl/arjenwiersma/aoc/days/Day01.java
@@ -8,12 +8,40 @@ public class Day01 implements Day<Integer> {
 
     @Override
     public Integer part1(List<String> input) {
-        return 0;
+        int larger = 0;
+        Integer last = null;
+
+        int[] nums = input.stream().mapToInt(x -> Integer.parseInt(x)).toArray();
+
+        for (int s : nums) {
+
+            if (last != null && s > last) {
+                larger++;
+            }
+
+            last = s;
+        }
+        return larger;
     }
 
     @Override
     public Integer part2(List<String> input) {
-        return 0;
+        int larger = 0;
+
+        int[] nums = input.stream().mapToInt(x -> Integer.parseInt(x)).toArray();
+
+        Integer last = null;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int sum = nums[i] + nums[i + 1] + nums[i + 2];
+
+            if (last != null && sum > last) {
+                larger++;
+            }
+
+            last = sum;
+        }
+        
+        return larger;
     }
     
 }
--- aoc-2021/src/test/java/nl/arjenwiersma/aoc/days/Day01Test.java
+++ aoc-2021/src/test/java/nl/arjenwiersma/aoc/days/Day01Test.java
@@ -1,8 +1,11 @@
 package nl.arjenwiersma.aoc.days;
 
-
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -13,16 +16,40 @@ public class Day01Test {
     @Rule
     public DayInputExternalResource input = new DayInputExternalResource(01);
 
+    Day<Integer> day;
+    
+    @Before
+    public void before() {
+        day = new Day01();
+    }
+
+    @Test
+    public void testSample() {
+        List<String> input = new ArrayList<>() {{
+                add("199");
+                add("200");
+                add("208");
+                add("210");
+                add("200");
+                add("207");
+                add("240");
+                add("269");
+                add("260");
+                add("263");
+            }
+            };
+
+        assertEquals(Integer.valueOf(7), day.part1(input));
+        assertEquals(Integer.valueOf(5), day.part2(input));
+    }
+
     @Test
     public void part1() {
-        Day<Integer> day = new Day01();
-        assertEquals(Integer.valueOf(0), day.part1(input.getLines()));
+        assertEquals(Integer.valueOf(1676), day.part1(input.getLines()));
     }
     
     @Test
     public void part2() {
-        Day<Integer> day = new Day01();
-        assertEquals(Integer.valueOf(0), day.part2(input.getLines()));        
+        assertEquals(Integer.valueOf(1706), day.part2(input.getLines()));
     }
 }
-
