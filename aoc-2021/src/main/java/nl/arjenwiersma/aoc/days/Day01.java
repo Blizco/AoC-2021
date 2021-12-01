9a0e7cd6939117a3048e84d58509f969767cb1d1
Arjen Wiersma <arjen@wiersma.org>
2021-12-01 13:02:53 +0100

    :tada: Day 01 solution after student discussions

--- aoc-2021/src/main/java/nl/arjenwiersma/aoc/days/Day01.java
+++ aoc-2021/src/main/java/nl/arjenwiersma/aoc/days/Day01.java
@@ -1,47 +1,42 @@
 package nl.arjenwiersma.aoc.days;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import nl.arjenwiersma.aoc.common.Day;
+import nl.arjenwiersma.aoc.utils.SlidingWindowSpliterator;
+
+import static nl.arjenwiersma.aoc.utils.SlidingWindowSpliterator.windowed;
 
 public class Day01 implements Day<Integer> {
-
     @Override
     public Integer part1(List<String> input) {
-        int larger = 0;
-        Integer last = null;
-
-        int[] nums = input.stream().mapToInt(x -> Integer.parseInt(x)).toArray();
-
-        for (int s : nums) {
-
-            if (last != null && s > last) {
-                larger++;
-            }
-
-            last = s;
-        }
-        return larger;
+        int[] n = input.stream().mapToInt(Integer::parseInt).toArray();
+        return getCountIncreased(n, 1);
     }
 
     @Override
     public Integer part2(List<String> input) {
-        int larger = 0;
+        int[] n = input.stream().mapToInt(Integer::parseInt).toArray();
+        return getCountIncreased(n, 3);
+    }
 
-        int[] nums = input.stream().mapToInt(x -> Integer.parseInt(x)).toArray();
+    int getCountIncreased(int[] n, int step) {
+        int howmany = 0;
+        for (int i = step; i < n.length; ++i) {
+            int sum1 = 0;
+            int sum2 = 0;
 
-        Integer last = null;
-        for (int i = 0; i < nums.length - 2; i++) {
-            int sum = nums[i] + nums[i + 1] + nums[i + 2];
-
-            if (last != null && sum > last) {
-                larger++;
+            for (int x = -1 * step; x < 0; x++) {
+                sum1 += n[i + x];
+                sum2 += n[i + (x+1)];
             }
 
-            last = sum;
+            if (sum1 < sum2) {
+                howmany++;
+            }
         }
-        
-        return larger;
+        return howmany;
     }
     
 }
