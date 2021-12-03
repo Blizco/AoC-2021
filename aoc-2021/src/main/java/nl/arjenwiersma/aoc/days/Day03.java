3cf4d218c30e4b352837db20b2854a32a5b84918
Arjen Wiersma <arjen@wiersma.org>
2021-12-03 13:54:45 +0100

    :tada: actually day 3 :D

--- aoc-2021/src/main/java/nl/arjenwiersma/aoc/days/Day03.java
+++ aoc-2021/src/main/java/nl/arjenwiersma/aoc/days/Day03.java
@@ -1,2 +1,89 @@
-package nl.arjenwiersma.aoc.days;public class Day03 {
+package nl.arjenwiersma.aoc.days;
+
+import nl.arjenwiersma.aoc.common.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day03 implements Day<Long> {
+    @Override
+    public Long part1(List<String> input) {
+        StringBuilder gamma = new StringBuilder();
+        StringBuilder epsilon = new StringBuilder();
+
+        int[] pos = new int[input.get(0).length()];
+        for (String in : input) {
+            for (int i = 0; i < in.length(); i++) {
+                pos[i] += (in.charAt(i) - 48);
+            }
+        }
+
+        for (int p : pos) {
+            int half = input.size() / 2;
+            if (p > half) {
+                gamma.append("1");
+                epsilon.append("0");
+            } else {
+                gamma.append("0");
+                epsilon.append("1");
+            }
+        }
+//        System.out.println(gamma);
+//        System.out.println(epsilon);
+        // loop over pos, maak gamma / epsilon strings
+
+        return Long.valueOf(gamma.toString(), 2) * Long.valueOf(epsilon.toString(), 2);
+    }
+
+    public List<String> findSubset(List<String> set, int index, boolean dominant) {
+        List<String> zeros = new ArrayList<>();
+        List<String> ones = new ArrayList<>();
+
+        int count = 0;
+
+        for (String in : set) {
+            //count += in.charAt(index) == '1' ? 1 : 0;
+            int x = (in.charAt(index) - 48);
+            count += x;
+            if (x == 1) {
+                ones.add(in);
+            } else {
+                zeros.add(in);
+            }
+        }
+
+        if (dominant && count >= (set.size() - count)) {
+            return ones;
+        } else if (!dominant && count < (set.size() - count)) {
+            return ones;
+        } else {
+            return zeros;
+        }
+    }
+
+    @Override
+    public Long part2(List<String> input) {
+        int oxygen = 0, co2 = 0;
+
+        List<String> current = input;
+        for (int i = 0; i < input.get(0).length(); i++) {
+            current = findSubset(current, i, true);
+            //System.out.println(current);
+            if (current.size() == 1)
+                break;
+        }
+
+        oxygen = Integer.valueOf(current.get(0), 2);
+
+        current = input;
+        for (int i = 0; i < input.get(0).length(); i++) {
+            current = findSubset(current, i, false);
+            //System.out.println(current);
+            if (current.size() == 1)
+                break;
+        }
+        co2 = Integer.valueOf(current.get(0), 2);
+
+        return (long) oxygen * co2;
+    }
 }
--- aoc-2021/src/test/java/nl/arjenwiersma/aoc/days/Day03Test.java
+++ aoc-2021/src/test/java/nl/arjenwiersma/aoc/days/Day03Test.java
@@ -1,2 +1,102 @@
-package nl.arjenwiersma.aoc.days;public class Day03Test {
+package nl.arjenwiersma.aoc.days;
+
+import nl.arjenwiersma.aoc.common.Day;
+import nl.arjenwiersma.aoc.common.DayInputExternalResource;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Day03Test {
+    @Rule
+    public DayInputExternalResource input = new DayInputExternalResource(3);
+
+    Day<Long> day;
+
+    @Before
+    public void before() {
+        day = new Day03();
+    }
+
+    @Test
+    public void testSample() {
+        List<String> input = new ArrayList<>() {{
+            add("00100");
+            add("11110");
+            add("10110");
+            add("10111");
+            add("10101");
+            add("01111");
+            add("00111");
+            add("11100");
+            add("10000");
+            add("11001");
+            add("00010");
+            add("01010");
+        }};
+
+        assertEquals(Long.valueOf(198), day.part1(input));
+        assertEquals(Long.valueOf(230), day.part2(input));
+    }
+
+    @Test
+    public void testSorting() {
+        List<String> input = new ArrayList<>() {{
+            add("00100");
+            add("11110");
+            add("10110");
+            add("10111");
+            add("10101");
+            add("01111");
+            add("00111");
+            add("11100");
+            add("10000");
+            add("11001");
+            add("00010");
+            add("01010");
+        }};
+
+        List<String> current = input;
+        for (int index = 0; index < input.get(0).length(); index++) {
+            final int x = index;
+            var sorted =
+                    current.stream().sorted((a, b) -> Integer.compare(b.charAt(x), a.charAt(x)))
+                            .collect(Collectors.groupingBy(
+                                    a -> a.charAt(x)
+                            ))
+                            .entrySet()
+                            .stream()
+                            .sorted(Map.Entry.comparingByValue((a,b) -> Integer.compare(b.size(), a.size())))
+                            .limit(1)
+                            .collect(Collectors.toList())
+                    ;
+
+            current = sorted.get(0).getValue();
+            System.out.println(current);
+        }
+        // buckets van 1 en 0
+//        for (String in : sorted) {
+//            // split 1 en 0
+//        }
+        // geef lijst met meest / minste count
+    }
+
+    @Test
+    public void testPart1() {
+        assertEquals(Long.valueOf(3277364), day.part1(input.getLines()));
+    }
+
+    @Test
+    public void testPart2() {
+        assertEquals(Long.valueOf(5736383), day.part2(input.getLines()));
+    }
 }
+
